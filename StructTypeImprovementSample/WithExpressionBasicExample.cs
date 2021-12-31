7d78dcac448cc67689ebed1c01cc46ffc9085b97
omid <ahmadpooromid@gmail.com>
2021-12-31 20:46:27 +0330

    add example for With Expression.

---
+++ StructTypeImprovementSample/WithExpressionBasicExample.cs
@@ -0,0 +1,32 @@
+﻿namespace StructTypeImprovementSample
+{
+    public class WithExpressionBasicExample
+    {
+        public record NamedPoint(string Name, int X, int Y);
+
+        public static void Main()
+        {
+            var p1 = new NamedPoint("A", 0, 0);
+            Console.WriteLine($"{nameof(p1)}: {p1}");  // output: p1: NamedPoint { Name = A, X = 0, Y = 0 }
+
+            var p2 = p1 with { Name = "B", X = 5 };
+            Console.WriteLine($"{nameof(p2)}: {p2}");  // output: p2: NamedPoint { Name = B, X = 5, Y = 0 }
+
+            var p3 = p1 with
+            {
+                Name = "C",
+                Y = 4
+            };
+            Console.WriteLine($"{nameof(p3)}: {p3}");  // output: p3: NamedPoint { Name = C, X = 0, Y = 4 }
+
+            Console.WriteLine($"{nameof(p1)}: {p1}");  // output: p1: NamedPoint { Name = A, X = 0, Y = 0 }
+
+            var apples = new { Item = "Apples", Price = "1.19" };
+            Console.WriteLine($"original apples: {apples}");
+
+            var saleApples = apples with { Price = "0.79" };
+            Console.WriteLine($"sale apples: {saleApples}");
+
+        }
+    }
+}
